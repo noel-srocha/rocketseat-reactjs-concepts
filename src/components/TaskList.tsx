62e2dd1a12260df74d8bd2e96a9c966e1ec65999
Manoel de Souza Rocha Neto <manoelrocha.neto.93@hotmail.com>
2022-02-12 19:29:37 -0300

    Implements simple ToDo List features

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,28 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {
+      setTasks([...tasks, {
+        id: Math.random() * 100,
+        title: newTaskTitle,
+        isComplete: false
+      }]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTask = tasks.map((task) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
+    setTasks(updatedTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList: Task[] = tasks.filter((task) => task.id !== id);
+    setTasks(newTaskList);
   }
 
   return (
